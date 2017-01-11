6285699a8f199244cb78c876ca103edca67b5a87
Iggy <igoririanto@rocketmail.com>
2017-01-11 17:22:18 -0500

    add proptypes and prompt

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,10 +1,16 @@
 var React = require('react');
+var transparentBg = require('../styles').transparentBg;
+var Link = require('react-router').Link;
 
 var Home = React.createClass({
   render: function(){
     return(
-      <div>
-        <h2>Hello from home!</h2>
+      <div className="jumbotron col-sm-12 text-center" style={transparentBg}>
+        <h2>GH Battle!</h2>
+        <p className="lead">(Some fancy motto here)</p>
+        <Link to="/playerOne">
+          <button type="button" className="btn btn-lg btn-success">Git Started</button>
+        </Link>
       </div>
     )
   }
