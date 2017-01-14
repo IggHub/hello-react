0761ac95b6f3fef645264efb352d37134f8fda2b
Iggy <igoririanto@rocketmail.com>
2017-01-13 18:08:21 -0800

    use axios to fetch GH API info

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -1,10 +1,13 @@
 //stateless functional component
 var React = require('react');
 
+function barf (object) {
+  return <pre>{JSON.stringify(object, null, '  ')}</pre>
+}
+
 function ConfirmBattle (props) {
   return props.isLoading == true
-  ? <p>Loading!</p>
-  : <p>Confirm battle now!</p>
+  ? <p>Loading!</p> : <div>Confirm battle now! {barf(props)}</div>
 }
 
 module.exports = ConfirmBattle;
--- app/containers/ConfirmBattleContainer.js
+++ app/containers/ConfirmBattleContainer.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var ConfirmBattle = require('../components/ConfirmBattle');
+var githubHelpers = require('../utils/githubHelpers');
 
 var ConfirmBattleContainer = React.createClass({
   contextTypes: {
@@ -12,20 +13,16 @@ var ConfirmBattleContainer = React.createClass({
       playersInfo: [],
     }
   },
-  componentWillMount: function(){
-    console.log('componentWillMount');
-  },
-  componentWillReceiveProps: function(){
-    console.log('componentWillReceiveProps');
-  },
-  componentWillUnmount: function(){
-    console.log('componentWillUnmount');
-  },
-  componentDidMount: function(){
+
+  componentDidMount: function () {
     var query = this.props.location.query;
-    console.log('componentDidMount');
-    // console.log('QUERY', query)
-    // needs to get info from github then update state
+    githubHelpers.getPlayersInfo([query.playerOne, query.playerTwo])
+      .then(function (players) {
+        this.setState({
+          isLoading: false,
+          playersInfo: [players[0], players[1]]
+        })
+      }.bind(this))
   },
   render: function(){
     return(
